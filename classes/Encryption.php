861c0fd3389d86449afa03b1b6999203ec60cc1d
oaviles <oaviles@sudespacho.net>
2018-08-22 02:48:05 -0400

    encryption to config

--- classes/Encryption.php
+++ classes/Encryption.php
@@ -1,14 +1,15 @@
 <?php
 //Encryption CLASS
 class Encryption {
-	private static $hash = "z`7^9G_Y7HGs,L48mEgHZe5D#?Fba]M=";
 	private static $method = "AES-128-CBC";
 	public static function encrypt($txt) {
-		return @openssl_encrypt ( $txt , self::$method , self::$hash);
+		global $config;
+		return @openssl_encrypt ( $txt , self::$method , $config['hash']);
 	}
 	
 	public static function decrypt($txt) {
-		return @openssl_decrypt ( $txt , self::$method , self::$hash);
+		global $config;
+		return @openssl_decrypt ( $txt , self::$method , $config['hash']);
 	}
 }
 ?>	
