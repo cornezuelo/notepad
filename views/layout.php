ed517f1421ba2f5429d361fc07b73abeea742abf
Oscar Aviles Miramontes <emeeseka01@gmail.com>
2018-11-13 11:43:21 +0100

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -2,7 +2,7 @@
 <html>
   <head>
 <?php if (isset($config['recaptchaPublicKey']) && !empty($config['recaptchaPublicKey'])) { ?>
-<script src='https://www.google.com/recaptcha/api.js?render=<?php echo $config['recaptchaPublicKey'] ?>'></script>	 
+<script src='https://www.google.com/recaptcha/api.js'></script>	 
 <?php } ?>
     <script src="resources/js/jquery-3.2.1.min.js"></script>
     <script src="resources/js/bootstrap.bundle.min.js"></script>
