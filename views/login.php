9890d160a5a6fc3e41885dfe02f16785445a2734
Oscar Aviles Miramontes <emeeseka01@gmail.com>
2018-11-13 11:44:37 +0100

    Update login.php
    implementamos recaptcha

--- views/login.php
+++ views/login.php
@@ -2,6 +2,9 @@
   <div class="container">
 	<p class="login-box-msg">Login inserting your password</p>
 	<form action="index.php?accion=login" method="post">          
+<?php if (isset($config['recaptchaPublicKey']) && !empty($config['recaptchaPublicKey'])) { ?>
+		<div class="g-recaptcha" data-sitekey="<?php echo $config['recaptchaPublicKey']; ?>"></div>
+<?php } ?>	
 	  <div class="form-group has-feedback">
 		<input type="password" name="password" class="form-control" placeholder="Password">
 		<span class="glyphicon glyphicon-lock form-control-feedback"></span>
