c1ce27443bf7ef4cfcbacfa530fa1f63979e3495
Oscar Aviles Miramontes <emeeseka01@gmail.com>
2018-11-13 11:56:22 +0100

    Update login.php

--- views/login.php
+++ views/login.php
@@ -2,15 +2,15 @@
   <div class="container">
 	<p class="login-box-msg">Login inserting your password</p>
 	<form action="index.php?accion=login" method="post">          
-<?php if (isset($config['recaptchaPublicKey']) && !empty($config['recaptchaPublicKey'])) { ?>
-		<div class="g-recaptcha" data-sitekey="<?php echo $config['recaptchaPublicKey']; ?>"></div>
-<?php } ?>	
 	  <div class="form-group has-feedback">
 		<input type="password" name="password" class="form-control" placeholder="Password">
 		<span class="glyphicon glyphicon-lock form-control-feedback"></span>
 	  </div>
 	  <div class="container">
 		<div class="col-xs-8">
+			<?php if (isset($config['recaptchaPublicKey']) && !empty($config['recaptchaPublicKey'])) { ?>
+					<div class="g-recaptcha" data-sitekey="<?php echo $config['recaptchaPublicKey']; ?>"></div>
+			<?php } ?>				
 			<?php if (isset($flag_pwd)) { ?>
 			<div align="center"><p style="color:red"><b>The password is wrong</b></p></div>
 			<?php } ?>
